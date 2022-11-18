716258e67a9249da3cad44ce45d51e8c4240900b
Petri Heinonen <petri@vaadin.com>
2022-11-18 13:51:48 +0200

    Changed push mode to LongPolling

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@PWA(name = "KitsTest", shortName = "KitsTest", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
-@Push
+@Push(transport = Transport.LONG_POLLING)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
