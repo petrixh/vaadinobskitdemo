e24469a1901fe7405deb67b8e7d7bf24187e0f05
Petri Heinonen <petri@vaadin.com>
2022-11-18 10:41:10 +0200

    Moved agent-jar download to target folder..

--- observability-kit/startObservabilityNewRelic.sh
+++ observability-kit/startObservabilityNewRelic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #TODO move files and paths to variables for better maintenance...
-AGENT_JAR_PATH=./observability-kit/agent/
+AGENT_JAR_PATH=./target/
 AGENT_JAR=vaadin-opentelemetry-javaagent-1.0.0.rc1.jar
 
 ##Exit hook for cleanup...
@@ -13,7 +13,7 @@ trap 'onExit' EXIT
 ##Bring down containers on Ctrl + c
 trap 'onExit' 2
 
-cd agent
+cd ../target
 echo "Checking for agent JAR and downloading if necessary"
 if [ -f "$AGENT_JAR" ]; then
   echo "Agent JAR already downloaded..."
@@ -25,5 +25,5 @@ cd ..
 echo "App on port 8080, new relic on https://one.eu.newrelic.com/"
 echo "Starting demo app in 3 seconds..."
 sleep 3s
-java -javaagent:./observability-kit/agent/vaadin-opentelemetry-javaagent-1.0.0.rc1.jar      -Dotel.javaagent.configuration-file=./observability-kit/agent-configs/agent-new-relic.properties  -jar ./target/kitstest-1.0-SNAPSHOT.jar
+java -javaagent:./target/vaadin-opentelemetry-javaagent-1.0.0.rc1.jar      -Dotel.javaagent.configuration-file=./observability-kit/agent-configs/agent-new-relic.properties  -jar ./target/kitstest-1.0-SNAPSHOT.jar
 
