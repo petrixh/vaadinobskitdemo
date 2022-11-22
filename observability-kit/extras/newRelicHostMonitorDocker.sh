5664b3c51c5e81aa32599713070b9a69add020a3
Petri Heinonen <petri@vaadin.com>
2022-11-22 15:04:27 +0200

    flip boolean for enabling process metrics, fixed newRelic host monitor script (sort of..)

--- observability-kit/extras/newRelicHostMonitorDocker.sh
+++ observability-kit/extras/newRelicHostMonitorDocker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo "Installing NewRelic Infra Host monitor in docker image... If you have changed the configs or want a newer version of the agent you should force a rebuild of the image..."
 echo "Do you want to force a rebuild of the docker image? y/N?"
+cd new-relic-host-infra-monitor
 select yn in "Yes" "No"; do
   case $yn in
-    Yes ) docker-compose up -d --build
-    No ) docker-compse up -d
+    Yes ) docker-compose up -d --build; break;;
+    No ) docker-compse up -d; break;;
   esac
 done
+cd ..
