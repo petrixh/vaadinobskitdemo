30c765943f43d559cda34fe697d7a4d3d41d9605
Petri Heinonen <petri@vaadin.com>
2025-07-11 15:34:43 +0300

    Added descriptions to custom spans example

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -2,9 +2,12 @@ package com.example.application.views.helloworld;
 
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,12 +18,14 @@ import io.opentelemetry.api.trace.Tracer;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
-public class HelloWorldView extends HorizontalLayout {
+public class HelloWorldView extends VerticalLayout {
 
     private TextField name;
     private Button sayHello;
 
     public HelloWorldView() {
+        add(new H1("Custom span/attribute example"));
+        add(new Text("Look for a span 'My Custom Span' and/or a span attribute: 'hello.value' with the value from the TextField")); 
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
@@ -37,10 +42,10 @@ public class HelloWorldView extends HorizontalLayout {
         });
         sayHello.addClickShortcut(Key.ENTER);
 
+        HorizontalLayout hl = new HorizontalLayout(name, sayHello); 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        hl.setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        add(hl);
     }
 
 }
